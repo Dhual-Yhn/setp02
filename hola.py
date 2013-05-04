fbabf9bca7e3cb91bb701f9205d6999c07445d26
Eduardo Mercader <eamercade@gmail.com>
2013-05-03 23:36:27 -0400

    Commit inicial

---
+++ hola.py
@@ -0,0 +1,9 @@
+# Set de problemas #2
+# Problema 1.
+# Lenguaje y Tecnicas de Programacion
+# Profesor: Igor Caracci
+# Profesor(Ayudante): Andres Caro
+# Universidad de Santiago de Chile
+# 03 de abril del 2013
+
+print("Hola, mundo")
